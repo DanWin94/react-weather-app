c6a4c5577ccc04290139ff6e6ffaceddaade4561
DanWin94 <dannynguyen49@yahoo.com>
2019-09-05 10:06:27 -0400

    first commit

---
+++ src/components/title.js
@@ -0,0 +1,13 @@
+import React from'react';
+
+
+const Title =(props)=>{
+    return(
+        <div>
+            <h1> Weather App</h1>
+            <p>Helps you find weather conditions in cities....</p>
+        </div>
+    )
+}
+
+export default Title;
